67edac70654e812aebdbc88b4a47f3b0d35825fb
freethinkel <freethinkel@gmail.com>
2020-01-22 18:13:42 +0300

    add classes

---
+++ lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var smirnov = new HeadOfDepartment();
+      smirnov.Walk();
+      string a = Console.ReadLine();
+      Console.WriteLine(a);
+    }
+  }
+
+
+  interface Person
+  {
+    void Walk() { }
+  }
+
+  class HeadOfDepartment : Person
+  {
+    void Walk()
+    {
+      Console.WriteLine("im walk!");
+    }
+  }
+
+  class Teacher
+  {
+
+  }
+
+  class Student { }
+
+}
